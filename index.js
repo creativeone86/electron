7ca9c97e0aa56deac77955acd834102946653aa2
Yordan Georgiev <iordan340@gmail.com>
2018-03-20 17:58:11 +0200

    new project

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const electron = require('electron');
-const {app, BrowserWindow, ipcMain} = electron;
-const ffmpeg = require('fluent-ffmpeg');
+const {
+	app,
+	BrowserWindow,
+	Menu
+} = electron;
+const menuTemplate = [
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'Add task'
+			},
+			{
+				label: 'Quit'
+			}
+		]
+	}
+];
+
 let mainWindow;
 
 app.on('ready', () => {
-	console.log('App is now ready.');
-
 	mainWindow = new BrowserWindow({});
+	mainWindow.loadURL(`file://${__dirname}/main.html`);
 	mainWindow.maximize();
-	mainWindow.openDevTools();
 
-	mainWindow.loadURL(`file://${__dirname}/index.html`);
-});
-
-ipcMain.on('video:submit', (event, path) => {
-	ffmpeg.ffprobe(path, (err, metadata) => {
-		mainWindow.webContents.send('video:metadata', metadata.format.duration);
-	});
+	const mainMenu = Menu.buildFromTemplate(menuTemplate);
+	Menu.setApplicationMenu(mainMenu);
 });
